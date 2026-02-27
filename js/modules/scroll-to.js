637dcf531f82be18c168e8ac2e748437e0f5f82a
Jo Muncaster <j_muncaster208947@fanshaweonline.ca>
2026-02-27 12:29:12 -0500

    Changes to index.html and scroll-to
    Changes made to 2 files to attempt to bug fix.

--- js/modules/scroll-to.js
+++ js/modules/scroll-to.js
@@ -1,21 +1,22 @@
-// SCROLL TO PROJECTS
-export function scrollTo() {
-    gsap.registerPlugin(ScrollToPlugin);
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin.js";
 
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+export function scrollTo() {
     const heroArrow = document.querySelector("#hero-arrow");
+
     if (heroArrow) {
         heroArrow.addEventListener("click", function () {
             gsap.to(window, {
                 duration: 1.5,
                 scrollTo: {
                     y: "#project-scroll",
-                    offsetY: 100,
-                    ease: "power1.out"
-                }
+                    offsetY: 100
+                },
+                ease: "power1.out"
             });
         });
     }
-
-// GSAP PAGE ANIMATIONS
-    gsap.registerPlugin(ScrollTrigger);
 }
